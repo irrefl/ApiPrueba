c26856f93cdb8df24ea76a4bcb639dff85c3f999
irrefl <ariel.reyes.flores@gmail.com>
2023-07-31 01:51:14 -0600

    new change in repository wrapper

--- src/DataAccess/Repositories/RepositoryWrapper.cs
+++ src/DataAccess/Repositories/RepositoryWrapper.cs
@@ -8,29 +8,9 @@ namespace DataAccess.Repositories
         private IFruitRepository _fruits;
         private IFruitTypeRepository _fruitTypes;
 
-        public IFruitRepository Fruits
-        {
-            get
-            {
-                if (_fruits == null)
-                {
-                    _fruits = new FruitRepository(_repoContext);
-                }
-                return _fruits;
-            }
-        }
+     public IFruitRepository Fruits => _fruits ??= new FruitRepository(_repoContext);
 
-        public IFruitTypeRepository FruitTypes
-        {
-            get
-            {
-                if (_fruitTypes == null)
-                {
-                    _fruitTypes = new FruitTypeRepository(_repoContext);
-                }
-                return _fruitTypes;
-            }
-        }
+     public IFruitTypeRepository FruitTypes => _fruitTypes ??= new FruitTypeRepository(_repoContext);
 
         public RepositoryWrapper(FruitContext r)
         {
